0717f1d4f9fcf291cb9e772403cc940f41ffc2fe
thetejasmagade <53045273+thetejasmagade@users.noreply.github.com>
2025-01-04 20:54:25 +0530

    added: manage/login link to test

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import { RouterLink } from 'vue-router';
 const hello = ref(JSON.parse(localStorage.getItem("data") || "null")?.items || [])
 </script>
 
 <template>
     <h1>home</h1>
+    <RouterLink to="/manage">
+        manage
+    </RouterLink>
+    <RouterLink to="/login">login</RouterLink>
     {{ hello }}
 </template>
