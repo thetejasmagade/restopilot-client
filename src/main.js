f2210855846eadc8f1e479513a50bf791e9abe6d
thetejasmagade <53045273+thetejasmagade@users.noreply.github.com>
2025-01-04 20:51:02 +0530

    inital commit

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import { createPinia } from 'pinia';
-import "./assets/index.css";
-import App from "./App.vue";
-import { router } from "./router/index";
-import '@fontsource/inter/400.css'; // This will load the default font weights and styles
-const pinia = createPinia();
-createApp(App).use(router).use(pinia).mount("#app");
